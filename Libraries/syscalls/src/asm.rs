c7b2b51a8c92f87289392f1c3da88ed02e9457eb
Noel <noel.carrascoit@gmail.com>
2024-06-26 17:37:37 +0200

    direct_syscall tests passed

--- Libraries/syscalls/src/asm.rs
+++ Libraries/syscalls/src/asm.rs
@@ -1,7 +1,8 @@
-#![feature(asm)]
 #![feature(global_asm)]
 
 use std::arch::global_asm;
+
+#[cfg(target_arch = "x86_64")]
 global_asm!(
     ".intel_syntax noprefix",
     ".data",
@@ -17,51 +18,33 @@ global_asm!(
     "    mov DWORD PTR wSystemCall[rip], r8d",
     "    ret",
 
-    ".global prepare_shellcode",
-    "prepare_shellcode:",
-    "    xor eax, eax",                        // Clear RAX (set it to 0)
-    "    mov al, dil",                         // Move the 8-bit value from DIL to AL (zero-extend to 64-bit)
-    "    mov DWORD PTR wSystemCall[rip], eax", // Store the 32-bit value in EAX into wSystemCall using RIP-relative addressing
-    "    ret",
-
-    ".global run_syscall",
-    "run_syscall:",
+    ".global run_direct_syscall",
+    "run_direct_syscall:",
     "    xor r10, r10",                 // r10 = 0
     "    mov rax, rcx",                 // rax = rcx
     "    mov r10, rax",                 // r10 = rax = rcx
     "    mov eax, DWORD PTR wSystemCall[rip]",  // eax = ssn
-    "    jmp Run",                      // execute 'Run'
+    "    jmp run",                      // execute 'Run'
     "    xor eax, eax",                 // won't run
     "    xor rcx, rcx",                 // won't run
     "    shl r10, 2",                   // won't run
-    "Run:",
+    "run:",
     "    syscall",                      // syscall
     "    ret"
 
 );
 
 extern "C" {
-    pub fn set_ssn(ssn: u8);
-    pub fn run_syscall(...);
+    pub fn set_ssn(ssn: usize);
+    pub fn run_direct_syscall(...) -> usize;
 }
 
 #[cfg(test)]
 mod private_tests {
-    use std::ffi::c_void;
-    use std::ptr::null;
-    use windows::Win32::Foundation::HANDLE;
-    use windows::Win32::System::Memory::{MEM_COMMIT, MEM_RESERVE, PAGE_READWRITE};
     use super::*;
 
     #[test]
     fn test_set_ssn() {
-        unsafe { set_ssn(0x0001u8); }
+        unsafe { set_ssn(0x18); }
     }
-
-/*    #[test]
-    fn test_run_syscall() {
-        unsafe {
-            run_syscall(HANDLE(-1), null() as *mut *mut c_void, 0, 32 as *mut usize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-        }
-    }*/
 }
--- Libraries/syscalls/src/lib.rs
+++ Libraries/syscalls/src/lib.rs
@@ -1,132 +1,26 @@
-mod asm;
-pub use asm::run_syscall;
+pub mod asm;
+pub use asm::run_direct_syscall;
+use asm::set_ssn;
+pub mod hells_gate;
+use hells_gate::{fetch_nt_syscall};
 
-use windows;
-use std::collections::HashMap;
-use utils::{get_dll_exported_functions_by_hash, get_export_directory};
-use utils::hash::{FastCrc32};
-use windows::Win32::Foundation::HMODULE;
-use windows::Win32::System::Threading::PEB;
-use windows::Win32::System::WindowsProgramming::LDR_DATA_TABLE_ENTRY;
-
-const NT_DLL_CRC32: u32 = 0x6030EF91u32;
-
-#[repr(C)]
-#[derive(Debug)]
-struct NtdllConfig {
-    pdw_array_of_addresses: *mut u32, // The VA of the array of addresses of ntdll's exported functions   [BaseAddress + IMAGE_EXPORT_DIRECTORY.AddressOfFunctions]
-    pdw_array_of_names: *mut u32,     // The VA of the array of names of ntdll's exported functions       [BaseAddress + IMAGE_EXPORT_DIRECTORY.AddressOfNames]
-    pw_array_of_ordinals: *mut u16,   // The VA of the array of ordinals of ntdll's exported functions    [BaseAddress + IMAGE_EXPORT_DIRECTORY.AddressOfNameOrdinals]
-    dw_number_of_names: u32,          // The number of exported functions from ntdll.dll                 [IMAGE_EXPORT_DIRECTORY.NumberOfNames]
-    u_module: usize,                  // The base address of ntdll - required to calculate future RVAs   [BaseAddress]
-}
-type PNtdllConfig = *mut NtdllConfig;
-
-static NT_DLL_CONFIG: NtdllConfig = NtdllConfig::new()?;
-
-impl NtdllConfig {
-    pub fn new() -> Option<Self> {
-        unsafe {
-            // Getting PEB
-            let p_peb: *mut PEB = utils::get_peb();
-            if p_peb.is_null() { // || (*p_peb).OSMajorVersion != 0xA
-                return None;
-            }
-
-            // Getting ntdll.dll module
-            let p_ldr_data = (*(*p_peb).Ldr).InMemoryOrderModuleList.Flink;
-            let p_ldr = ((*p_ldr_data).Flink as *mut u8).sub(0x10) as *mut LDR_DATA_TABLE_ENTRY; //skip local image element
-            let u_module = (*p_ldr).DllBase as usize;
-            if u_module == 0 {
-                return None;
-            }
-
-            // Fetching the export directory of ntdll
-            let h_module = HMODULE(u_module as isize);
-            let p_img_exp_dir = get_export_directory(h_module)?;
-
-            // Initializing the NtdllConfig struct
-            let config = NtdllConfig {
-                u_module,
-                dw_number_of_names: (*p_img_exp_dir).NumberOfNames,
-                pdw_array_of_names: (u_module + (*p_img_exp_dir).AddressOfNames as usize) as *mut u32,
-                pdw_array_of_addresses: (u_module + (*p_img_exp_dir).AddressOfFunctions as usize) as *mut u32,
-                pw_array_of_ordinals: (u_module + (*p_img_exp_dir).AddressOfNameOrdinals as usize) as *mut u16,
-            };
-
-            // Checking
-            if config.u_module == 0 || config.dw_number_of_names == 0 || config.pdw_array_of_names.is_null() || config.pdw_array_of_addresses.is_null() || config.pw_array_of_ordinals.is_null() {
-                None
-            } else {
-                Some(config)
-            }
+/// Prepares a system call by fetching the NT syscall using the provided hash.
+///
+/// # Parameters
+///
+/// - `hash`: A 32-bit unsigned integer representing the hash value used to fetch the NT syscall.
+///
+/// # Panics
+///
+/// This function will panic if there is an error in fetching the NT syscall.
+/// The error message will be printed with the hash value.
+pub unsafe fn prepare_syscall(hash: u32) {
+    match fetch_nt_syscall(hash) {
+        Ok(syscall) => {
+            set_ssn(syscall.dw_ssn as usize);
+        },
+        Err(e) => {
+            panic!("[prepare_syscall] Error: {}", e);
         }
     }
 }
-
-#[repr(C)]
-pub struct NtSyscall {
-    dw_ssn: u32,
-    dw_syscall_hash: u32,
-    p_syscall_address: *mut std::ffi::c_void,
-}
-pub type PntSyscall = *mut NtSyscall;
-
-
-pub unsafe fn prepare_syscall(hash: u32){
-    // Implementation of FetchNtSyscall
-    // Uses the _NT_SYSCALL struct
-    // use static NT_DLL_CONFIG instance
-}
-
-#[test]
-fn test_direct_syscall() {
-    match NtdllConfig::new() {
-        Some(config) => println!("NtdllConfig initialized: {:?}", config),
-        None => println!("Failed to initialize NtdllConfig"),
-    }
-}
-
-unsafe fn crawl_nt_dll(){
-    let mut syscalls = HashMap::new();
-    let crc32 = FastCrc32::new();
-
-    // This is a placeholder for the actual enumeration and hashing logic
-    // Fill syscalls with NtSyscall structs
-    // 1. fetches all the exported functions from ntdll.dll
-    let exported_functions = get_dll_exported_functions_by_hash(NT_DLL_CRC32 as usize);
-    // . computes the hashes for the name functions
-    // . creates an entry for the NtApiFunc
-    for function in exported_functions {
-        let hash = crc32.compute_hash(function.to_uppercase().as_bytes());
-        syscalls.insert(hash, Syscall { ssn: 0, p_syscall_address: 0});
-    }
-}
-
-
-
-struct Syscall {
-    pub ssn: u8, // The syscall number
-    pub p_syscall_address: u8, // The address of the syscall
-}
-
-pub struct NtApiFunc {
-    syscalls: HashMap<u32, Syscall>, // crc32 hash, NtSyscall
-}
-
-
-/*pub unsafe fn new() -> Self {
-
-
-    NtApiFunc { syscalls }
-}
-
-pub fn get_function_ssn(function_hash: u32) -> Option<u8>
-{
-    syscalls.get(&function_hash).map(|syscall| syscall.ssn)
-}
-
-pub fn get_function_syscall_address(function_hash: u32) -> Option<u8>
-{
-    syscalls.get(&function_hash).map(|syscall| syscall.p_syscall_address)
-}*/
